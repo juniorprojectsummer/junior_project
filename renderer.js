ab7929afe33a11cf4536afd253619648b66d7acf
Yasir Khan <yasir_khan15@hotmail.com>
2017-05-24 09:43:40 +0300

    login added, navigation added

--- renderer.js
+++ renderer.js
@@ -4,44 +4,44 @@
 
 
 var mysql = require('mysql');
-var bcrypt = require('bcrypt');
+// var bcrypt = require('bcrypt');
 var ipc = electron.ipcRenderer
 
 
-function createConnection () {
+// function createConnection () {
 
-	// Add the credentials to access your database
-	var connection = mysql.createConnection({
-	    host     : 'localhost',
-	    user     : 'root',
-	    password : '', // or the original password : 'apaswword'
-	    database : 'feed'
-	});
+// 	// Add the credentials to access your database
+// 	var connection = mysql.createConnection({
+// 	    host     : 'localhost',
+// 	    user     : 'root',
+// 	    password : '', // or the original password : 'apaswword'
+// 	    database : 'feed'
+// 	});
 
-	// connect to mysql
-	connection.connect(function(err) {
-	    // in case of error
-	    if(err){
-	        console.log(err.code);
-	        console.log(err.fatal);
-	    }
-	});
-	// Perform a query
-	$query = 'SELECT * FROM `user` LIMIT 10';
+// 	// connect to mysql
+// 	connection.connect(function(err) {
+// 	    // in case of error
+// 	    if(err){
+// 	        console.log(err.code);
+// 	        console.log(err.fatal);
+// 	    }
+// 	});
+// 	// Perform a query
+// 	$query = 'SELECT * FROM `user` LIMIT 10';
 
-	connection.query($query, function(err, rows, fields) {
-	    if(err){
-	        console.log("An error ocurred performing the query.");
-	        console.log(err);
-	        return;
-	    }
+// 	connection.query($query, function(err, rows, fields) {
+// 	    if(err){
+// 	        console.log("An error ocurred performing the query.");
+// 	        console.log(err);
+// 	        return;
+// 	    }
 
-	    console.log("Query succesfully executed", rows);
-	});
+// 	    console.log("Query succesfully executed", rows);
+// 	});
 
-	// Close the connection
-	connection.end(function(){
-	    // The connection has been closed
-	});
+// 	// Close the connection
+// 	connection.end(function(){
+// 	    // The connection has been closed
+// 	});
 
-}
+// }
